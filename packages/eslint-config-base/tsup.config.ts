a81177cafc58b4f3787fac6495817c7ca8b1869d
Sean Blonien <seanb2016@gmail.com>
2026-01-07 22:26:56 -0600

    fix: Target es2022 for builds

--- packages/eslint-config-base/tsup.config.ts
+++ packages/eslint-config-base/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
+  target: 'es2022',
   dts: true,
   clean: true,
   sourcemap: true,
